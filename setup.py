d3af33f4291ddb1baf582b07739160ab1362d7e2
Landry Simo <landrysimo99@gmail.com>
2024-10-22 10:30:40 +0100

    add setups

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='paroles-net-wrapper',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/Starland9/paroles-net-wrapper.git',
+    license='MIT',
+    author='Starland9',
+    author_email='landrysimo99@gmail.com',
+    description='Ce projet permet de récupérer les paroles de chansons de paroles.net.'
+)
