a289a072a41331e9c055958e0e4a567bb08d69de
Landry Simo <landrysimo99@gmail.com>
2025-08-24 04:25:32 +0100

    Updates package version to 0.0.6
    Updates the package version to reflect changes in the codebase.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='paroles-net-wrapper',
-    version='0.0.5',
+    version='0.0.6',
     packages=find_packages(),
     py_modules=['paroles_net', 'utils'],
     install_requires=[
